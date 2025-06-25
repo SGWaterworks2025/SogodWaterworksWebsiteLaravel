ae09481a8c9a7ec5a5dad5dcba4a5770951b279a
SGWaterworks2025 <sogodwaterworks2025@gmail.com>
2025-06-25 08:25:44 +0800

    Add Bootstrap integration with assets and dependencies; update package.json and app.css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
-import './bootstrap';
+import './bootstrap';       // if you scaffolded bootstrap.js already
+import 'bootstrap';         // this pulls in the JS + Popper
 
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
---
+++ resources/views/includes/bootstrap-assets.blade.php
@@ -0,0 +1,9 @@
+<!-- resources/views/includes/bootstrap-assets.blade.php -->
+<link
+  rel="stylesheet"
+  href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}"
+>
+<script
+  src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"
+  defer
+></script>
